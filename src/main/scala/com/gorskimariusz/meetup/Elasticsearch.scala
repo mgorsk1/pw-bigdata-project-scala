a11dd816a43b1f8e6281f495f240853d09c24e6d
mgorsk1 <gorskimariusz13@gmail.com>
2018-11-30 11:45:29 +0100

    refactor(elasticsearch client): column with time naming

--- src/main/scala/com/gorskimariusz/meetup/Elasticsearch.scala
+++ src/main/scala/com/gorskimariusz/meetup/Elasticsearch.scala
@@ -7,18 +7,20 @@ import org.apache.spark.sql.types.DoubleType
 
 object Elasticsearch {
   def index(data: DataFrame, index: String, prefix: Option[String]): Unit = {
-    val dataWithTimestamp = data.withColumn("@timestamp", unix_timestamp().cast(DoubleType)*1000)
+    val dataWithTimestamp = data.withColumn("timestamp", unix_timestamp().cast(DoubleType)*1000)
 
     var fullIndexName = ""
 
     if (prefix.isDefined) {
       val fullPrefix = prefix.get
       fullIndexName = s"$index-{$fullPrefix}/default"
+
+      dataWithTimestamp.saveToEs(fullIndexName, Map("es.mapping.exclude" -> fullPrefix))
     }
     else {
       fullIndexName = s"$index/default"
-    }
 
-    dataWithTimestamp.saveToEs(fullIndexName, Map("es.mapping.exclude" -> "dateForIndex"))
+      dataWithTimestamp.saveToEs(fullIndexName)
+    }
   }
 }
